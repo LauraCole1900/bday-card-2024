6f4106cc9a1b18649fc29ecc0152f4a6b8adb551
Laura Cole <lauracole1900@comcast.net>
2024-11-02 01:19:10 -0600

    adding photos, carousel component & overlay component

---
+++ src/components/Carousel.jsx/index.jsx
@@ -0,0 +1,10 @@
+import './style.css';
+
+const Carousel = () => {
+
+  return (
+    <p>This is the photo carousel thingy</p>
+  )
+};
+
+export default Carousel;
---
+++ src/components/Overlay/index.jsx
@@ -0,0 +1,9 @@
+import './style.css';
+
+const Overlay = () => {
+  return (
+    <p>Hi, this is the overlay</p>
+  )
+};
+
+export default Overlay;
