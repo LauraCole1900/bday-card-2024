6c0ae5bf8decacde152d4d069195acab02447b3b
Laura Cole <lauracole1900@comcast.net>
2024-11-02 00:06:40 -0600

    changing favicon, removing default guts of App.jsx

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,13 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/bday-card-2024",
+  build: {
+    outDir: 'build',
+  },
   plugins: [react()],
+  server: {
+    port: 3000,
+    open: true
+  }
 })
